9510e82b680ac118a9ed9fea8fe7de5525566e43
Thiago <thia.mdossantos@gmail.com>
2017-08-07 20:23:35 -0300

    more experiments

---
+++ src/multiple_returns.py
@@ -0,0 +1,7 @@
+def move(x, y, positionsToMove):
+    return x + positionsToMove, y + positionsToMove
+
+x, y = move(3, 5, 2)
+
+print('x:', x)
+print('y:', y)
---
+++ src/scope.py
@@ -0,0 +1,9 @@
+x = 4
+
+def localScope():
+    x = 3
+    print(x)
+
+
+localScope()
+print(x)
